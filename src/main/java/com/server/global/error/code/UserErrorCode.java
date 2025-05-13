57e3b8f65242e7c1e7a4415274d2192f22fb8ba9
Zerohertz <ohg3417@gmail.com>
2025-05-13 23:19:34 +0900

    :hammer: fix: 401 when user not found (related: #78)
    Signed-off-by: Zerohertz <ohg3417@gmail.com>

--- src/main/java/com/server/global/error/code/UserErrorCode.java
+++ src/main/java/com/server/global/error/code/UserErrorCode.java
@@ -8,11 +8,13 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum UserErrorCode implements ErrorCode {
-    NOT_FOUND(HttpStatus.NOT_FOUND.value(), "User not found."), INVALID_PARAMETER(
-            HttpStatus.BAD_REQUEST.value(), "Invalid parameter."), ALREADY_ACTIVE(
-                    HttpStatus.BAD_REQUEST.value(), "Account is already active."), RESTORATION_PERIOD_EXPIRED(
-                            HttpStatus.GONE.value(), "Account restoration period has expired.");
+        NOT_FOUND(HttpStatus.UNAUTHORIZED.value(), "User not found."), INVALID_PARAMETER(
+                        HttpStatus.BAD_REQUEST.value(),
+                        "Invalid parameter."), ALREADY_ACTIVE(HttpStatus.BAD_REQUEST.value(),
+                                        "Account is already active."), RESTORATION_PERIOD_EXPIRED(
+                                                        HttpStatus.GONE.value(),
+                                                        "Account restoration period has expired.");
 
-    private final int status;
-    private final String message;
+        private final int status;
+        private final String message;
 }
