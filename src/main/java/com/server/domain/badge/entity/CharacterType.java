6c5a4ec5945e1b714eb283d99d824b23ec29bf5f
jaeseung choun <jaeseung_choun@tmax.co.kr>
2025-07-06 18:45:20 +0900

    feat: Badge 관련 엔티티

---
+++ src/main/java/com/server/domain/badge/entity/CharacterType.java
@@ -0,0 +1,17 @@
+package com.server.domain.badge.entity;
+
+
+public enum CharacterType {
+    RABBIT("토끼"),
+    BEAR("곰돌이");
+
+    private final String displayName;
+
+    CharacterType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
---
+++ src/main/java/com/server/domain/badge/entity/UserBadge.java
@@ -0,0 +1,46 @@
+package com.server.domain.badge.entity;
+
+import com.server.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserBadge {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "badge_id")
+    private Badge badge;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "character_type")
+    private CharacterType characterType;
+
+    @Column(nullable = false)
+    private Boolean isMain = false;
+
+    @Column(name = "created_at", nullable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
