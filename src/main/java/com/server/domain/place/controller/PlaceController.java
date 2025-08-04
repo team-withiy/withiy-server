c3f4e7002a870353b59ddc175c7eb3b7ffe3d6a2
jaeseung choun <jaeseung_choun@tmax.co.kr>
2025-08-05 02:45:10 +0900

    chore: Swagger API 주석 추가

--- src/main/java/com/server/domain/place/controller/PlaceController.java
+++ src/main/java/com/server/domain/place/controller/PlaceController.java
@@ -9,11 +9,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
--- src/main/java/com/server/domain/place/dto/CreatePlaceResponse.java
+++ src/main/java/com/server/domain/place/dto/CreatePlaceResponse.java
@@ -2,6 +2,7 @@ package com.server.domain.place.dto;
 
 import com.server.domain.category.dto.CategoryDto;
 import com.server.domain.place.entity.Place;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,9 +10,13 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class CreatePlaceResponse {
+    @Schema(description = "장소 ID")
     private Long id;
+    @Schema(description = "장소 이름")
     private String name;
+    @Schema(description = "장소 주소")
     private String address;
+    @Schema(description = "장소 카테고리")
     private CategoryDto category;
 
     @Builder
