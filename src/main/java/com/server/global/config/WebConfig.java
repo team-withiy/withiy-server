61bd393b3e24c4b279421289384967b9ff0b6e95
jaeseung choun <jaeseung_choun@tmax.co.kr>
2025-08-09 17:06:23 +0900

    fix: 다중 Origin 허용

--- src/main/java/com/server/global/config/WebConfig.java
+++ src/main/java/com/server/global/config/WebConfig.java
@@ -16,7 +16,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+            .allowedOrigins(
+                "http://localhost:3000",
+                "https://withiy.zerohertz.xyz",
+                "https://withiy-prod.zerohertz.xyz"
+            )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
