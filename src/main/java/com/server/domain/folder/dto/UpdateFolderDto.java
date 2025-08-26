618e38af02f996af94c5034b9f0eccc3bb3ef5bb
jaeseung choun <jaeseung_choun@tmax.co.kr>
2025-08-27 03:37:49 +0900

    refactor: 요청 DTO Swagger 스키마 주석 추가

--- src/main/java/com/server/domain/folder/dto/UpdateFolderDto.java
+++ src/main/java/com/server/domain/folder/dto/UpdateFolderDto.java
@@ -1,6 +1,7 @@
 package com.server.domain.folder.dto;
 
 import com.server.domain.folder.entity.FolderColor;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,8 +13,11 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Schema(description = "폴더 수정 요청 DTO")
 public class UpdateFolderDto {
 
+	@Schema(description = "폴더 이름", example = "맛집")
 	private String name;
+	@Schema(description = "폴더 색상", example = "RED")
 	private FolderColor color;
 }
