bc58059379a6a23f23f2d8af1aab1dae0e522602
jaeseung choun <jaeseung_choun@tmax.co.kr>
2025-09-03 19:20:36 +0900

    docs: Swagger 스키마 주석 적용

--- src/main/java/com/server/global/pagination/dto/ApiCursorPaginationRequest.java
+++ src/main/java/com/server/global/pagination/dto/ApiCursorPaginationRequest.java
@@ -1,5 +1,6 @@
 package com.server.global.pagination.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,9 +10,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Schema(description = "커서 기반 페이지네이션 요청")
 public class ApiCursorPaginationRequest implements PaginationRequest {
 
+	@Schema(description = "커서 위치, null일 경우 첫 페이지", example = "123")
 	private Long cursor;
+	@Schema(description = "한 번에 가져올 데이터 수", example = "10", defaultValue = "10")
 	private int limit;
+	@Schema(description = "이전 페이지로 이동 여부, false일 경우 다음 페이지", example = "false", defaultValue = "false")
 	private Boolean prev;
 }
