f33e18dea8ec60d8aea7e43ee399b9d83a9e407f
jaeseung choun <jaeseung_choun@tmax.co.kr>
2025-11-03 09:30:13 +0900

    fix: 커플 조회 로직 수정
    - 커플 서비스에서 커플 조회하도록 변경

--- src/main/java/com/server/domain/album/service/AlbumFacade.java
+++ src/main/java/com/server/domain/album/service/AlbumFacade.java
@@ -9,7 +9,9 @@ import com.server.domain.album.dto.AlbumPageResponse;
 import com.server.domain.album.dto.AlbumResponse;
 import com.server.domain.album.entity.Album;
 import com.server.domain.album.entity.AlbumComment;
+import com.server.domain.user.entity.Couple;
 import com.server.domain.user.entity.User;
+import com.server.domain.user.service.CoupleService;
 import com.server.global.error.exception.BusinessException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -20,70 +22,76 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class AlbumFacade {
-    private final AlbumService albumService;
-    private final AlbumCommentService albumCommentService;
 
-    public AlbumPageResponse getAlbums(User user, int page, int size, String order)
-            throws Exception {
-        Page<Album> albumPage = albumService.getAlbums(user.getCouple(), page, size, order);
-        List<AlbumResponse> albumResponses = albumPage.get().toList()
-                .stream()
-                .map(this::createAlbumResponse)
-                .toList();
+	private final AlbumService albumService;
+	private final AlbumCommentService albumCommentService;
+	private final CoupleService coupleService;
 
-        return new AlbumPageResponse(albumResponses, albumPage.getTotalElements());
-    }
+	public AlbumPageResponse getAlbums(User user, int page, int size, String order)
+		throws Exception {
+		Couple couple = coupleService.getCoupleOrNull(user);
+		Page<Album> albumPage = albumService.getAlbums(couple, page, size, order);
+		List<AlbumResponse> albumResponses = albumPage.get().toList()
+			.stream()
+			.map(this::createAlbumResponse)
+			.toList();
 
-    public AlbumDetailResponse getAlbum(User user, Long albumId) {
-        Album album = albumService.getAlbum(user.getCouple(), albumId);
-        AlbumResponse albumResponse = createAlbumResponse(album);
-        List<AlbumCommentResponse> comments = album.getAlbumComments()
-                .stream()
-                .map(ac -> new AlbumCommentResponse(ac.getId(), ac.getComment()))
-                .toList();
-        return new AlbumDetailResponse(albumResponse, comments);
-    }
+		return new AlbumPageResponse(albumResponses, albumPage.getTotalElements());
+	}
 
-    @Transactional
-    public void writeComment(User user, AlbumCommentRequest request) {
-        Album album = albumService.getAlbum(user.getCouple(), request.getAlbumId());
-        List<AlbumComment> albumComments = album.getAlbumComments();
-        int cnt = 0;
-        for (AlbumComment albumComment : albumComments) {
-            if (albumComment.getUser().getId().equals(user.getId())) {
-                cnt++;
-            }
-        }
+	public AlbumDetailResponse getAlbum(User user, Long albumId) {
+		Couple couple = coupleService.getCoupleOrNull(user);
+		Album album = albumService.getAlbum(couple, albumId);
+		AlbumResponse albumResponse = createAlbumResponse(album);
+		List<AlbumCommentResponse> comments = album.getAlbumComments()
+			.stream()
+			.map(ac -> new AlbumCommentResponse(ac.getId(), ac.getComment()))
+			.toList();
+		return new AlbumDetailResponse(albumResponse, comments);
+	}
 
-        if (cnt == 10) {
-            throw new BusinessException(ALBUM_COMMENT_OVER_10);
-        }
+	@Transactional
+	public void writeComment(User user, AlbumCommentRequest request) {
+		Couple couple = coupleService.getCoupleOrNull(user);
+		Album album = albumService.getAlbum(couple, request.getAlbumId());
+		List<AlbumComment> albumComments = album.getAlbumComments();
+		int cnt = 0;
+		for (AlbumComment albumComment : albumComments) {
+			if (albumComment.getUser().getId().equals(user.getId())) {
+				cnt++;
+			}
+		}
 
-        AlbumComment albumComment = AlbumComment.builder()
-                .album(album)
-                .user(user)
-                .comment(request.getComment())
-                .build();
-        albumComments.add(albumComment);
-    }
+		if (cnt == 10) {
+			throw new BusinessException(ALBUM_COMMENT_OVER_10);
+		}
 
-    @Transactional
-    public void updateComment(User user, Long commentId, AlbumCommentRequest request) {
-        Album album = albumService.getAlbum(user.getCouple(), request.getAlbumId());
-        AlbumComment albumComment = albumCommentService.getAlbumComment(commentId, album, user);
-        albumComment.updateComment(request.getComment());
-    }
+		AlbumComment albumComment = AlbumComment.builder()
+			.album(album)
+			.user(user)
+			.comment(request.getComment())
+			.build();
+		albumComments.add(albumComment);
+	}
 
-    @Transactional
-    public void deleteComment(User user, Long commentId) {
-        albumCommentService.deleteAlbumComment(commentId, user);
-    }
+	@Transactional
+	public void updateComment(User user, Long commentId, AlbumCommentRequest request) {
+		Couple couple = coupleService.getCoupleOrNull(user);
+		Album album = albumService.getAlbum(couple, request.getAlbumId());
+		AlbumComment albumComment = albumCommentService.getAlbumComment(commentId, album, user);
+		albumComment.updateComment(request.getComment());
+	}
 
-    private AlbumResponse createAlbumResponse(Album album) {
-        List<String> photoUrl = album.getAlbumPhotos().stream()
-                .map(albumPhoto -> albumPhoto.getPhoto().getImgUrl())
-                .toList();
+	@Transactional
+	public void deleteComment(User user, Long commentId) {
+		albumCommentService.deleteAlbumComment(commentId, user);
+	}
 
-        return new AlbumResponse(album.getId(), album.getTitle(), album.getScheduleAt(), photoUrl);
-    }
+	private AlbumResponse createAlbumResponse(Album album) {
+		List<String> photoUrl = album.getAlbumPhotos().stream()
+			.map(albumPhoto -> albumPhoto.getPhoto().getImgUrl())
+			.toList();
+
+		return new AlbumResponse(album.getId(), album.getTitle(), album.getScheduleAt(), photoUrl);
+	}
 }
