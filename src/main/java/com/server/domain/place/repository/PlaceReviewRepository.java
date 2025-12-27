f460d557fc316a23c62f4bc2b04d359120bffaec
jaeseung choun <jaeseung_choun@tmax.co.kr>
2025-12-27 17:16:00 +0900

    refactor: 장소/사진/리뷰 서비스 및 쿼리 개선
    - AlbumFacade, DateSchedService 등 연관 서비스 네이밍 및 쿼리 정리

--- src/main/java/com/server/domain/place/repository/PlaceReviewRepository.java
+++ src/main/java/com/server/domain/place/repository/PlaceReviewRepository.java
@@ -3,11 +3,22 @@ package com.server.domain.place.repository;
 import com.server.domain.place.entity.Place;
 import com.server.domain.place.entity.PlaceReview;
 import com.server.domain.user.entity.User;
+import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PlaceReviewRepository extends JpaRepository<PlaceReview, Long> {
     Optional<PlaceReview> findByPlaceAndUser(Place place, User user);
+
+    @Query("SELECT pr FROM PlaceReview pr " +
+        "JOIN FETCH pr.place p " +
+        "JOIN FETCH pr.user u " +
+        "WHERE pr.place.id IN :placeIds " +
+        "ORDER BY pr.id DESC")
+    List<PlaceReview> findRecentReviewsByPlaceIds(@Param("placeIds") List<Long> placeIds, Pageable pageable);
 }
